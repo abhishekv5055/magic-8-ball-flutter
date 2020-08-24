d7ac2fa571bf2c8eebca5a65c77bf7263d7fd293
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-08-24 16:30:50 +0530

    AppBar created

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue[500],
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text('Ask Me Anything'),
+          ),
+        ),
       ),
     );
