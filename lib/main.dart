46a95123f5219fdd949a056a46c341e032c0c782
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-08-24 16:41:39 +0530

    add the images to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -8,6 +9,24 @@ void main() => runApp(
             backgroundColor: Colors.blue,
             title: Text('Ask Me Anything'),
           ),
+          body: AskMeAnything(),
         ),
       ),
     );
+
+class AskMeAnything extends StatefulWidget {
+  @override
+  _AskMeAnythingState createState() => _AskMeAnythingState();
+}
+
+class _AskMeAnythingState extends State<AskMeAnything> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(24.0),
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
